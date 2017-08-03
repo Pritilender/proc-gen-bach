5091026c621498acb63b58276afd8d55492b25b5
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-08-03 11:15:36 -0700

    Add boxer library for cross-platform message boxes.

--- src/includes.h
+++ src/includes.h
@@ -9,6 +9,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <GLFW/glfw3.h>
+#include <boxer/boxer.h>
 
 // Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
 // To use stb_image, add this in *one* C++ source file.
