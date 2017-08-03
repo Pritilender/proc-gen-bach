c6cb9bd6eae79b483ef659ea8418e56e30a5f839
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-08-03 10:11:26 -0700

    Simplify file structure.

--- src/includes.h
+++ src/includes.h
@@ -8,8 +8,13 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <GLFW/glfw3.h>
 
 // Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
 // To use stb_image, add this in *one* C++ source file.
 //     #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
